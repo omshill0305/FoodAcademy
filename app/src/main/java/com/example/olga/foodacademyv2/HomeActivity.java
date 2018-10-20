ebc906eb61ad7449ef47a16211ccd0c39481dcd9
Olga <omshil@mail.com>
2018-10-20 22:03:17 +0200

    NavigationFirstFix

--- app/src/main/java/com/example/olga/foodacademyv2/HomeActivity.java
+++ app/src/main/java/com/example/olga/foodacademyv2/HomeActivity.java
@@ -17,14 +17,20 @@ public class HomeActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_about:
+                    mTextMessage.setText(R.string.title_about);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_events:
+                    mTextMessage.setText(R.string.title_events);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_buchen:
+                    mTextMessage.setText(R.string.title_buchen);
+                    return true;
+                case R.id.navigation_route:
+                    mTextMessage.setText(R.string.title_route);
+                    return true;
+                case R.id.navigation_more:
+                    mTextMessage.setText(R.string.title_more);
                     return true;
             }
             return false;
