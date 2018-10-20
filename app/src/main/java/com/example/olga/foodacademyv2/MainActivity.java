11fc4f20ae93567b22da30fe5839170045e8a4f8
Olga <omshil@mail.com>
2018-10-20 21:44:37 +0200

    FirstCommit

--- app/src/main/java/com/example/olga/foodacademyv2/MainActivity.java
+++ app/src/main/java/com/example/olga/foodacademyv2/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.olga.foodacademyv2;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public class MainActivity extends Activity {
+        private static int SPLASH_TIME_OUT = 4000;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            requestWindowFeature(Window.FEATURE_NO_TITLE);
+            setContentView(R.layout.activity_main);
+            new Handler().postDelayed(new Runnable(){
+                @Override
+                public void run(){
+                    Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(homeIntent);
+                    finish();
+                }
+            }, SPLASH_TIME_OUT);
+
+        }
     }
-}
