7d11771b6648fc0ec90fcd016c9d92e9517be4ec
Olga <omshil@mail.com>
2018-10-22 16:34:08 +0200

    NewVersion

--- app/src/main/java/com/example/olga/foodacademyv2/HomeActivity.java
+++ app/src/main/java/com/example/olga/foodacademyv2/HomeActivity.java
@@ -1,50 +1,65 @@
 package com.example.olga.foodacademyv2;
 
-import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.widget.FrameLayout;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_about:
-                    mTextMessage.setText(R.string.titel_message);
-                    return true;
-                case R.id.navigation_events:
-                    mTextMessage.setText(R.string.title_events);
-                    return true;
-                case R.id.navigation_buchen:
-                    mTextMessage.setText(R.string.title_book_text);
-                    return true;
-                case R.id.navigation_route:
-                    mTextMessage.setText(R.string.title_route);
-                    return true;
-                case R.id.navigation_more:
-                    mTextMessage.setText(R.string.title_more);
-                    return true;
-            }
-            return false;
-        }
-    };
+    private BottomNavigationView mMainNav;
+    private FrameLayout mMainFrame;
+    private AboutFragment aboutFragment;
+    private BookFragment bookFragment;
+    private EventsFragment eventsFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        mMainFrame = (FrameLayout) findViewById(R.id.main_frame);
+        mMainNav = (BottomNavigationView) findViewById(R.id.main_nav);
+
+        aboutFragment = new AboutFragment();
+        eventsFragment = new EventsFragment();
+        bookFragment = new BookFragment();
+
+
+        mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_about:
+                        //mMainNav.setItemIconTint(R.color.colorAccent);
+                        setFragment(aboutFragment);
+                        return true;
+                    case R.id.nav_book:
+                        //mMainNav.setItemBackgroundResource(R.color.colorAccent);
+                        setFragment(bookFragment);
+                        return true;
+                    case R.id.nav_events:
+                        //mMainNav.setItemBackgroundResource(R.color.colorAccent);
+                        setFragment(eventsFragment);
+                        return true;
+
+                    default:
+                        return false;
+                }
+
+
+            }
+        });
     }
 
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
+    }
 }
