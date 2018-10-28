48e2465cf003b31c6c1fb98eb0c5d66129cd56fb
Olga <omshil@mail.com>
2018-10-28 13:36:40 +0100

    SaveDateFromFragment

--- app/src/main/java/com/example/olga/foodacademyv2/BookFragment.java
+++ app/src/main/java/com/example/olga/foodacademyv2/BookFragment.java
@@ -3,9 +3,12 @@ package com.example.olga.foodacademyv2;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
@@ -23,7 +26,18 @@ public class BookFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_book, container, false);
+
+        final View rootView = inflater.inflate(R.layout.fragment_book, container, false);
+        Button bt_send = (Button) rootView.findViewById(R.id.button);
+        bt_send.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                System.out.println("Hi its me");
+                EditText date = (EditText) rootView.findViewById(R.id.dateTE);
+                Log.i("Datum", date.getText().toString());
+            }
+        });
+        return rootView;
     }
 
 }
