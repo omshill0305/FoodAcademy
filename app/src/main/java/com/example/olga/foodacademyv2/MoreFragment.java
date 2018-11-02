44720e101783ec26fe2d8070102c71c77e565d2b
Olga <omshil@mail.com>
2018-11-02 08:38:05 +0100

    MoreDone

--- app/src/main/java/com/example/olga/foodacademyv2/MoreFragment.java
+++ app/src/main/java/com/example/olga/foodacademyv2/MoreFragment.java
@@ -18,7 +18,7 @@ import static com.example.olga.foodacademyv2.R.id.button_route;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MoreFragment extends Fragment {
+public class MoreFragment extends Fragment implements View.OnClickListener{
 
 
     public MoreFragment() {
@@ -31,16 +31,47 @@ public class MoreFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         final View rootView = inflater.inflate(R.layout.fragment_more, container, false);
-        Button clickedRoute = (Button) rootView.findViewById(R.id.button_route);
-        clickedRoute.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                Intent intent = new Intent(getActivity(), RouteActivity.class);
-                startActivity(intent);
-                System.out.println("I clicked route_button");
-            }
-        });
+        Button button1 = (Button) rootView.findViewById(R.id.button_gallery);
+        Button button2 = (Button) rootView.findViewById(R.id.button_speisekarte);
+        Button button3 = (Button) rootView.findViewById(R.id.button_feedback);
+        Button button4 = (Button) rootView.findViewById(R.id.button_route);
+        Button button5 = (Button) rootView.findViewById(R.id.button_about);
+
+
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+        button5.setOnClickListener(this);
+
         return rootView;
     }
 
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.button_gallery:
+                Intent intent = new Intent(getActivity(), GalleryActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.button_speisekarte:
+                Intent intent1 = new Intent(getActivity(), MenuActivity.class);
+                startActivity(intent1);
+                break;
+           case R.id.button_feedback:
+               Intent intent2 = new Intent(getActivity(), FeedbackActivity.class);
+                startActivity(intent2);
+                break;
+            case R.id.button_route:
+                Intent intent3 = new Intent(getActivity(), RouteActivity.class);
+                startActivity(intent3);
+                break;
+            case R.id.button_about:
+                Intent intent4 = new Intent(getActivity(), WirActivity.class);
+                startActivity(intent4);
+                break;
+
+
+            }
+    }
 }
