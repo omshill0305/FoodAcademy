98cd077c3d271347ba636c832b38fccbaf4c048e
Olga <omshil@mail.com>
2018-11-13 08:27:56 +0100

    BewertungOnClick

--- app/src/main/java/com/example/olga/foodacademyv2/FeedbackActivity.java
+++ app/src/main/java/com/example/olga/foodacademyv2/FeedbackActivity.java
@@ -1,5 +1,6 @@
 package com.example.olga.foodacademyv2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,17 +14,38 @@ public class FeedbackActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Klick hier, um deine Bewertung zu schreiben", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                View view = findViewById(R.id.feedback_layout_id);
+                String message= "";
+                int duration = Snackbar.LENGTH_LONG;
+
+                showSnackbar(view, message, duration);
+
+
             }
+
         });
     }
+     public void showSnackbar(View view, String message,int duration){
+         final Snackbar snackbar = Snackbar.make(view, message, duration);
+
+        snackbar.setAction("Klick hier, um deine Bewertung zu schreiben", new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 startActivity(new Intent(FeedbackActivity.this, WriteFeedbackActivity.class));
+
+
+             }
+         });
+
+         snackbar.show();
+     }
 
 }
