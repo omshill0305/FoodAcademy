3ea56f71c4619f6a36ab2438a9b1c0925cf81982
Olga <omshil@mail.com>
2018-11-22 12:14:11 +0100

    DBv1

--- app/src/main/java/com/example/olga/foodacademyv2/FeedbackActivity.java
+++ app/src/main/java/com/example/olga/foodacademyv2/FeedbackActivity.java
@@ -8,12 +8,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class FeedbackActivity extends AppCompatActivity {
+
+    DatabaseHelper myDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+        
+
+
+
+
+
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
