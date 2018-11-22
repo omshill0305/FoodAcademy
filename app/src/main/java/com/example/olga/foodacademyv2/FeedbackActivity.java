57fea83b7499688bf44337cd204ea2e85b70716f
Olga <omshil@mail.com>
2018-11-22 12:18:30 +0100

    DBv2

--- app/src/main/java/com/example/olga/foodacademyv2/FeedbackActivity.java
+++ app/src/main/java/com/example/olga/foodacademyv2/FeedbackActivity.java
@@ -27,11 +27,23 @@ public class FeedbackActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        myDB = new DatabaseHelper(this);
+
+        //populate an ArrayList<String> from the database and then view it
+        ArrayList<String> theList = new ArrayList<>();
+        Cursor data = myDB.getListContents();
+        if(data.getCount() == 0){
+            Toast.makeText(this, "There are no contents in this list!",Toast.LENGTH_LONG).show();
+        }else{
+            while(data.moveToNext()){
+                theList.add(data.getString(1));
+                ListAdapter listAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,theList);
+                listView.setAdapter(listAdapter);
+            }
+        }
         
-
-
-
-
 
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
