74352f91283fc16d24c4d34ce3b05245481c4a69
Olga <omshil@mail.com>
2018-11-22 11:16:39 +0100

    Revert "DatabaseV1"
    This reverts commit 551155fa88d4d919915ae4107df21a7daa0d3a9b.

--- app/src/main/java/com/example/olga/foodacademyv2/FeedbackActivity.java
+++ app/src/main/java/com/example/olga/foodacademyv2/FeedbackActivity.java
@@ -1,30 +1,19 @@
 package com.example.olga.foodacademyv2;
 
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
 
 public class FeedbackActivity extends AppCompatActivity {
-    DatabaseHelper myDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
-
-        ListView listView = (ListView) findViewById(R.id.listView);
-        myDB = new DatabaseHelper(this);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -43,19 +32,6 @@ public class FeedbackActivity extends AppCompatActivity {
             }
 
         });
-
-        //populate an ArrayList<String> from the database and then view it
-        ArrayList<String> theList = new ArrayList<>();
-        Cursor data = myDB.getListContents();
-        if(data.getCount() == 0){
-            Toast.makeText(this, "There are no contents in this list!",Toast.LENGTH_LONG).show();
-        }else{
-            while(data.moveToNext()){
-                theList.add(data.getString(1));
-                ListAdapter listAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,theList);
-                listView.setAdapter(listAdapter);
-            }
-        }
     }
      public void showSnackbar(View view, String message,int duration){
          final Snackbar snackbar = Snackbar.make(view, message, duration);
